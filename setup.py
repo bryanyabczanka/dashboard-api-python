f0523f2d7f2da9706ea8e6cdc6abff69000f2031
bryanyabczanka <bryan.yabczanka@gmail.com>
2019-09-03 11:22:23 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='meraki',
+    version='1.0.0',
+    url='https://github.com/bryanyabczanka/dashboard-api-python',
+    author='Blah',
+    author_email='author@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
+)
